dab4102bae6ee264ebf174004a34047e67d95fed
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-06-07 18:23:42 +0530

    done

--- pages/works/sizefy.js
+++ pages/works/sizefy.js
@@ -1,13 +1,11 @@
 import {
-  Box,
+ 
   Container,
   Badge,
   Link,
   List,
   ListItem,
-  SimpleGrid,
-  UnorderedList,
-  Image
+ 
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
